c79d2cbfe97b7c837e7065cebd557bdebb333f92
Valerian-4epik <98113328+Valerian-4epik@users.noreply.github.com>
2022-05-03 20:22:20 +0300

    v2

--- HomeWorkUI2/Assets/Skripts/Health.cs
+++ HomeWorkUI2/Assets/Skripts/Health.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Buttons : MonoBehaviour
+public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
 
+    private float _health = 100;
     private float _value = 10;
     private float _incrementStep = 1;
 
+    public float HealthValue => _health;
+
     public void ToDamage()
     {
-        _slider.value -= _value;
+        _health -= _value;
     }
 
     public void ToCure()
@@ -22,9 +25,9 @@ public class Buttons : MonoBehaviour
 
     private IEnumerator ChangeValue(float value)
     {
-        for(int i = 0; i < _value; i++)
+        for (int i = 0; i < _value; i++)
         {
-            _slider.value = Mathf.MoveTowards(_slider.value, _slider.value + _value, _incrementStep);
+            _health = Mathf.MoveTowards(_health, _health + _value, _incrementStep);
 
             yield return new WaitForSeconds(0.5f);
         }
--- HomeWorkUI2/Assets/Skripts/SliderScripts.cs
+++ HomeWorkUI2/Assets/Skripts/SliderScripts.cs
@@ -8,9 +8,16 @@ public class SliderScripts : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private Health _health;
     
     private void Start()
     {
         _slider.onValueChanged.AddListener((v) => { _text.text = v.ToString("0.00"); });
+        
+    }
+
+    private void Update()
+    {
+        _slider.value = _health.HealthValue;
     }
 }
